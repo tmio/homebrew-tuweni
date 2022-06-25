e510d15960d38f12c84a9ed77499e6146a470b3d
Antoine Toulme <antoine@lunar-ocean.com>
2022-06-24 22:16:17 -0700

    update Tuweni homebrew to 2.2.0

--- tuweni.rb
+++ tuweni.rb
@@ -1,16 +1,16 @@
 class Tuweni < Formula
   desc "apache tuweni ethereum client"
   homepage "https://github.com/apache/incubator-tuweni"
-  url "https://downloads.apache.org/incubator/tuweni/2.0.0-incubating/tuweni-bin-2.0.0-incubating.zip"
+  url "https://downloads.apache.org/incubator/tuweni/2.2.0-incubating/tuweni-bin-2.2.0-incubating.zip"
   # update with: ./updateTuweni.sh <new-version>
-  sha256 "a11f19509e14811e97065c283adeeaf171b6b5b79690403ce5e147b78b534a55"
+  sha256 "05a0f44672862b5ba1dc0b4515601b4c047e807d1823ba4a26d7131079e65506"
 
   depends_on "openjdk" => "11+"
 
   def install
     prefix.install "lib"
     bin.install "bin/tuweni"
-    # bin.install "bin/jsonrpc"
+    bin.install "bin/jsonrpc"
     bin.install "bin/eth-faucet"
   end
 
